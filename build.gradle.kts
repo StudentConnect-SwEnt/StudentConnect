6155b31b42c9d65dc638e7b7ce94ae81f73bf0fd
othmanehousni <othmane.housni@epfl.ch>
2025-10-02 14:20:44 +0200

    ci: set up Android CI and SonarCloud integration
    Configure GitHub Actions CI for Android.
    
    Add steps: ktfmtCheck, assemble, lint, unit and connected tests.
    
    Enable Gradle and AVD caching to speed up builds.
    
    Generate JaCoCo coverage and upload to SonarCloud.
    
    Add SonarQube Gradle plugin and set project key and organization.
    
    Use Temurin JDK 17.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,13 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
+    id("org.sonarqube") version "6.3.1.5724"
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+}
+
+sonar {
+  properties {
+    property("sonar.projectKey", "StudentConnect-SwEnt_StudentConnect")
+    property("sonar.organization", "studentconnect-swent")
+  }
 }
