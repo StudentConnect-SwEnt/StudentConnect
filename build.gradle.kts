5aa2d6006520dabc48a16714ce6d40ca03ae78e6
83759 <83759@users.noreply.github.com>
2025-10-03 03:08:39 +0200

    build: upgrade build configuration using bootcamp
    Upgrade the build configuration using the files from the SwEnt bootcamp.
    The build.grade.kts, app/build.gradle.kts, and gradle/libs.versions.toml
    files were updated. This makes sure all libraries and plugins are up to
    date.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("org.sonarqube") version "6.3.1.5724"
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    alias(libs.plugins.ktfmt) apply false
+    alias(libs.plugins.gms) apply false
+    id("org.sonarqube") version "6.3.1.5724" // not using alias() since it is incompatible with the sonarqube plugin
 }
 
 sonar {
