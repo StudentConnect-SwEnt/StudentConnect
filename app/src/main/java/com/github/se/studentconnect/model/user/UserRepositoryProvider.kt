6f8224f2b86a0c72aaec657a2e187d810dc02ed6
83759 <83759@users.noreply.github.com>
2025-10-09 01:53:07 +0200

    chore(user): add UserRepositoryProvider
    Add a single provider for UserRepository in the code.

--- app/src/main/java/com/github/se/studentconnect/model/user/UserRepositoryProvider.kt
+++ app/src/main/java/com/github/se/studentconnect/model/user/UserRepositoryProvider.kt
@@ -1,14 +1,10 @@
-package com.github.se.studentconnect.repository
+package com.github.se.studentconnect.model.user
 
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
-/**
- * Provides a single instance of the repository in the app. `repository` is mutable for testing
- * purposes.
- */
-object UserRepositoryProvider {
-  private val _repository: UserRepository = UserRepositoryFirestore(Firebase.firestore)
-
-  var repository: UserRepository = _repository
+/** Provides a single instance of the repository in the app. */
+class UserRepositoryProvider {
+    private val _repository: UserRepository = UserRepositoryFirestore(Firebase.firestore)
+    val repository: UserRepository = _repository
 }
