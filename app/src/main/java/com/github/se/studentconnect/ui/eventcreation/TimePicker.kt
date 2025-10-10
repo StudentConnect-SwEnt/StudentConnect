8c7eba930daa571579d8b92261639da4982c7c4e
83759 <83759@users.noreply.github.com>
2025-10-10 02:47:30 +0200

    chore(event): move TimePicker to separate file
    Move the TimePicker composable to a separate file to increase modularity
    and reusability.

---
+++ app/src/main/java/com/github/se/studentconnect/ui/eventcreation/TimePicker.kt
@@ -0,0 +1,38 @@
+package com.github.se.studentconnect.ui.eventcreation
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
+import java.time.LocalTime
+
+@Composable
+fun TimePicker(time: LocalTime, onTimeChange: (LocalTime) -> Unit) {
+    val context = LocalContext.current
+    val activity = context as AppCompatActivity
+
+    TextButton(
+        onClick = {
+            val picker =
+                MaterialTimePicker.Builder()
+                    .setTimeFormat(TimeFormat.CLOCK_24H)
+                    .setHour(time.hour)
+                    .setMinute(time.minute)
+                    .setTitleText("Select time")
+                    .build()
+
+            picker.addOnPositiveButtonClickListener {
+                onTimeChange(LocalTime.of(picker.hour, picker.minute))
+            }
+
+            picker.show(activity.supportFragmentManager, "timePicker")
+        }) {
+        val formattedHour = time.hour.toString().padStart(2, '0')
+        val formattedMinutes = time.minute.toString().padStart(2, '0')
+        Text(text = "${formattedHour}:${formattedMinutes}")
+    }
+}
+
