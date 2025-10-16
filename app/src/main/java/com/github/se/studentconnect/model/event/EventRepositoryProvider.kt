4a0a12ad9cf198983b9d0246b947a65c2b864d4f
tristan@archlaptop <tristan.bourquin@epfl.ch>
2025-10-16 04:39:26 +0200

    fix: change test to not use firebase

--- app/src/main/java/com/github/se/studentconnect/model/event/EventRepositoryProvider.kt
+++ app/src/main/java/com/github/se/studentconnect/model/event/EventRepositoryProvider.kt
@@ -90,5 +90,7 @@ object EventRepositoryProvider {
   fun date(seconds: Long): Timestamp =
       Timestamp(
           Date.from(
-              LocalDateTime.now().plusSeconds(seconds).atZone(ZoneId.systemDefault()).toInstant()))
+              LocalDateTime.now().plusSeconds(seconds).atZone(ZoneId.systemDefault()).toInstant()
+          )
+      )
 }
