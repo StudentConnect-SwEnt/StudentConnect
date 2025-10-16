54148e713c73e813facba9224fb6ce283104a432
83759 <83759@users.noreply.github.com>
2025-10-16 17:32:18 +0200

    fix(build): remove kotlinCompilerExtensionVersion
    Remove the deprecated kotlinCompilerExtensionVersion version option from
    build.gradle.kts.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,9 +52,6 @@ android {
     buildFeatures {
         compose = true
     }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.6.8"
-    }
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
