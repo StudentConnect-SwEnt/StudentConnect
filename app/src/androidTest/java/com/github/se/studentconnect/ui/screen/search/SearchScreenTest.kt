5f0e1c803ea5bad9a4760a9e0ee33a48cf50f018
tristan@archlaptop <tristan.bourquin@epfl.ch>
2025-10-16 18:15:08 +0200

    refactor: improve tests isolation

--- app/src/androidTest/java/com/github/se/studentconnect/ui/screen/search/SearchScreenTest.kt
+++ app/src/androidTest/java/com/github/se/studentconnect/ui/screen/search/SearchScreenTest.kt
@@ -1,22 +1,96 @@
 package com.github.se.studentconnect.ui.screen.search
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertIsNotDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performTextInput
+import com.github.se.studentconnect.model.User
+import com.github.se.studentconnect.model.event.Event
+import com.github.se.studentconnect.model.event.EventRepositoryLocal
+import com.github.se.studentconnect.model.event.EventRepositoryProvider
+import com.github.se.studentconnect.repository.UserRepositoryLocal
+import com.github.se.studentconnect.repository.UserRepositoryProvider
 import com.github.se.studentconnect.resources.C
 import com.github.se.studentconnect.ui.theme.AppTheme
+import com.google.firebase.Timestamp
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
 class SearchScreenTest {
 
+  private suspend fun initUserRepository() {
+    UserRepositoryProvider.repository = UserRepositoryLocal()
+
+    for (i in 1..10) {
+      val uid = "user$i"
+      val email = "user$i@epfl.ch"
+      val firstName = "FirstName$i"
+      val lastName = "LastName$i"
+      val university = "EPFL"
+      val createdAt = System.currentTimeMillis() - i * 100000L
+      val updatedAt = System.currentTimeMillis() - i * 50000L
+
+      UserRepositoryProvider.repository.saveUser(
+          User(
+              uid,
+              email,
+              firstName,
+              lastName,
+              university,
+              createdAt = createdAt,
+              updatedAt = updatedAt,
+          ))
+    }
+  }
+
+  private suspend fun initEventRepository() {
+    EventRepositoryProvider.repository = EventRepositoryLocal()
+
+    for (i in 1..10) {
+      val uid = "e$i"
+      val own = i % 3
+      val ownerId = "user$own"
+      val title = "Sample Event $i"
+      val description = "Description for event $i"
+      val start = Timestamp.now()
+      val isFlash = i % 2 == 0
+      val subtitle = "Subtitle for event $i"
+      EventRepositoryProvider.repository.addEvent(
+          Event.Public(
+              uid,
+              ownerId,
+              title,
+              description,
+              start = start,
+              isFlash = isFlash,
+              subtitle = subtitle,
+          ))
+    }
+  }
+
+  @Composable
+  private fun TestSearchScreen() {
+    val viewModel = SearchViewModel()
+    LaunchedEffect(Unit) { viewModel.init() }
+    SearchScreen(viewModel = viewModel)
+  }
+
   @get:Rule val composeTestRule = createComposeRule()
+
+  @Before
+  fun setup() = runTest {
+    initUserRepository()
+    initEventRepository()
+    composeTestRule.setContent { AppTheme { TestSearchScreen() } }
+  }
 
   @Test
   fun testSearchPageDisplayed() {
-    composeTestRule.setContent { AppTheme { TestSearchScreen() } }
     composeTestRule.onNodeWithTag(C.Tag.search_screen).assertIsDisplayed()
     composeTestRule.onNodeWithTag(C.Tag.back_button).assertIsDisplayed()
     composeTestRule.onNodeWithTag(C.Tag.search_input_field).assertIsDisplayed()
@@ -28,8 +102,6 @@ class SearchScreenTest {
 
   @Test
   fun testSearchUserDisplayed() {
-    composeTestRule.setContent { AppTheme { TestSearchScreen() } }
-
     composeTestRule.onNodeWithTag(C.Tag.search_input_field).performTextInput("user")
 
     composeTestRule.onNodeWithTag(C.Tag.search_screen).assertIsDisplayed()
@@ -43,8 +115,6 @@ class SearchScreenTest {
 
   @Test
   fun testSearchEventDisplayed() {
-    composeTestRule.setContent { AppTheme { TestSearchScreen() } }
-
     composeTestRule.onNodeWithTag(C.Tag.search_input_field).performTextInput("sample")
 
     composeTestRule.onNodeWithTag(C.Tag.search_screen).assertIsDisplayed()
@@ -58,8 +128,6 @@ class SearchScreenTest {
 
   @Test
   fun testSearchEventDisplayedWhenNotStart() {
-    composeTestRule.setContent { AppTheme { TestSearchScreen() } }
-
     composeTestRule.onNodeWithTag(C.Tag.search_input_field).performTextInput("event")
 
     composeTestRule.onNodeWithTag(C.Tag.search_screen).assertIsDisplayed()
@@ -73,8 +141,6 @@ class SearchScreenTest {
 
   @Test
   fun testSearchNothingDisplayed() {
-    composeTestRule.setContent { AppTheme { TestSearchScreen() } }
-
     composeTestRule.onNodeWithTag(C.Tag.search_input_field).performTextInput("nothing")
 
     composeTestRule.onNodeWithTag(C.Tag.search_screen).assertIsDisplayed()
