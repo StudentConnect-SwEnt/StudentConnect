6a7c45977dcd2a1013a0460b8794893be98d5f71
tristan@archlaptop <tristan.bourquin@epfl.ch>
2025-10-16 04:50:45 +0200

    refactor: comply with ktfmt standard

--- app/src/main/java/com/github/se/studentconnect/model/event/EventRepositoryProvider.kt
+++ app/src/main/java/com/github/se/studentconnect/model/event/EventRepositoryProvider.kt
@@ -90,7 +90,5 @@ object EventRepositoryProvider {
   fun date(seconds: Long): Timestamp =
       Timestamp(
           Date.from(
-              LocalDateTime.now().plusSeconds(seconds).atZone(ZoneId.systemDefault()).toInstant()
-          )
-      )
+              LocalDateTime.now().plusSeconds(seconds).atZone(ZoneId.systemDefault()).toInstant()))
 }
