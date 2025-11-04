99f7df1cb446431af0d83e7e117abb2bfae23eca
83759 <83759@users.noreply.github.com>
2025-11-04 10:45:23 +0100

    build: add fallback for reportPaths

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,10 +13,16 @@ sonar {
         property("sonar.projectName", "StudentConnect")
         property("sonar.organization", "studentconnect-swent")
         property("sonar.host.url", "https://sonarcloud.io")
+
         // Comma-separated paths to the various directories containing the *.xml JUnit report files. Each path may be absolute or relative to the project base directory.
-        property("sonar.junit.reportPaths", "${project(":app").layout.buildDirectory.get()}/test-results/testResOverrideDebugUnitTest/")
+        val unitTestResultDirectory =
+            if (project.hasProperty("useResOverride")) "testResOverrideDebugUnitTest"
+            else "testNormalDebugUnitTest"
+        property("sonar.junit.reportPaths", "${project(":app").layout.buildDirectory.get()}/test-results/${unitTestResultDirectory}/")
+
         // Paths to xml files with Android Lint issues. If the main flavor is changed, this file will have to be changed too.
         property("sonar.androidLint.reportPaths", "${project(":app").layout.buildDirectory.get()}/reports/lint-results-debug.xml")
+
         // Paths to JaCoCo XML coverage report files.
         property("sonar.coverage.jacoco.xmlReportPaths", "${project(":app").layout.buildDirectory.get()}/reports/jacoco/jacocoTestReport/jacocoTestReport.xml")
     }
