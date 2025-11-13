6676ed2a8875005dea5dbed047e946cc75d62353
elazdi-al <alielazdi@gmail.com>
2025-11-13 19:35:08 +0100

    chore: set target version to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,6 +23,7 @@ android {
     defaultConfig {
         applicationId = "com.github.se.studentconnect"
         minSdk = 29
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
