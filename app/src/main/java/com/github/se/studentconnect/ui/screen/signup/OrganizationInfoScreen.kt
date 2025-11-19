048aeaf62ff71915ca61a28346efa9cf114ecf7c
haaissa <haboubaissa@gmail.com>
2025-11-19 22:25:19 +0100

    fix(format) : ktfmtFormat

--- app/src/main/java/com/github/se/studentconnect/ui/screen/signup/OrganizationInfoScreen.kt
+++ app/src/main/java/com/github/se/studentconnect/ui/screen/signup/OrganizationInfoScreen.kt
@@ -23,7 +23,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.github.se.studentconnect.R
