18137cfecd521df53b8616f200a579126066ac0c
Rita Naimi <rita.naimi@epfl.ch>
2025-11-20 16:13:54 +0100

    chore(format): apply ktfmt formatting

--- app/src/main/java/com/github/se/studentconnect/ui/screen/signup/AccountTypeSelectionScreen.kt
+++ app/src/main/java/com/github/se/studentconnect/ui/screen/signup/AccountTypeSelectionScreen.kt
@@ -47,7 +47,6 @@ import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
@@ -360,11 +359,4 @@ private fun rememberAccountTypeMetrics(): AccountTypeMetrics {
       featureIconTextSpacing = featureTextSpacing,
       compactIconSize = compactIcon,
       heroIconSize = heroIcon)
-}
-
-@Preview(showBackground = true)
-@Composable
-private fun AccountTypeSelectionPreview() {
-  val previewViewModel = remember { SignUpViewModel() }
-  AccountTypeSelectionScreen(viewModel = previewViewModel, onContinue = {}, onBack = {})
 }
