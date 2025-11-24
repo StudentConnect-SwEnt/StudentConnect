4889d09885b7978688c4300502168f78aa6573ea
83759 <83759@users.noreply.github.com>
2025-11-24 23:48:15 +0100

    fix(build): use normal flavor for lint results

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,18 +16,20 @@ sonar {
         property("sonar.organization", "studentconnect-swent")
         property("sonar.host.url", "https://sonarcloud.io")
 
-        val flavor =
-            if (project.hasProperty("useResOverride")) "resOverride"
-            else "normal"
-
-        val flavorCapitalized = flavor.replaceFirstChar { it.titlecase(Locale.ROOT) }
+        val unitTestResultsFlavor =
+            if (project.hasProperty("useResOverrideForUnitTestResults")) "ResOverride"
+            else "Normal"
 
         // Comma-separated paths to the various directories containing the *.xml JUnit report files. Each path may be absolute or relative to the project base directory.
-        val unitTestResultDirectory = "test${flavorCapitalized}DebugUnitTest"
+        val unitTestResultDirectory = "test${unitTestResultsFlavor}DebugUnitTest"
         property("sonar.junit.reportPaths", "${project(":app").layout.buildDirectory.get()}/test-results/${unitTestResultDirectory}/")
 
+        val lintResultsFlavor =
+            if (project.hasProperty("useResOverrideForLintResults")) "resOverride"
+            else "normal"
+
         // Paths to xml files with Android Lint issues. If the main flavor is changed, this file will have to be changed too.
-        val lintResultsFile = "lint-results-${flavor}Debug.xml"
+        val lintResultsFile = "lint-results-${lintResultsFlavor}Debug.xml"
         property("sonar.androidLint.reportPaths", "${project(":app").layout.buildDirectory.get()}/reports/${lintResultsFile}")
 
         // Paths to JaCoCo XML coverage report files.
