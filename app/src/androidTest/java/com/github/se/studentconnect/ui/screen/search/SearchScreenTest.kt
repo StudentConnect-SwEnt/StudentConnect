13ea744c2132cae20db7f85a3f463de0275bfeff
TristanBour <tristan.bourquin@epfl.ch>
2025-11-26 23:21:58 +0100

    feat(test): add tests for new search screen

--- app/src/androidTest/java/com/github/se/studentconnect/ui/screen/search/SearchScreenTest.kt
+++ app/src/androidTest/java/com/github/se/studentconnect/ui/screen/search/SearchScreenTest.kt
@@ -1,10 +1,16 @@
 package com.github.se.studentconnect.ui.screen.search
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.test.assertAll
+import androidx.compose.ui.test.assertCountEquals
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertIsNotDisplayed
+import androidx.compose.ui.test.hasClickAction
+import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.isDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onAllNodesWithTag
+import androidx.compose.ui.test.onFirst
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performTextInput
 import com.github.se.studentconnect.model.User
@@ -13,13 +19,13 @@ import com.github.se.studentconnect.model.event.EventRepositoryLocal
 import com.github.se.studentconnect.model.event.EventRepositoryProvider
 import com.github.se.studentconnect.repository.UserRepositoryLocal
 import com.github.se.studentconnect.repository.UserRepositoryProvider
-import com.github.se.studentconnect.resources.C
 import com.github.se.studentconnect.ui.theme.AppTheme
 import com.google.firebase.Timestamp
 import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.mockito.kotlin.not
 
 class SearchScreenTest {
 
@@ -60,7 +66,7 @@ class SearchScreenTest {
       val ownerId = "user$own"
       val title = "Sample Event $i"
       val description = "Description for event $i"
-      val start = Timestamp.now()
+      val start = Timestamp(Timestamp.now().seconds - 2 + i, Timestamp.now().nanoseconds)
       val isFlash = i % 2 == 0
       val subtitle = "Subtitle for event $i"
       EventRepositoryProvider.repository.addEvent(
@@ -93,102 +99,175 @@ class SearchScreenTest {
   fun testSearchPageDisplayed() {
     composeTestRule.waitForIdle()
 
-    composeTestRule.onNodeWithTag(C.Tag.search_screen).assertIsDisplayed()
-    composeTestRule.onNodeWithTag(C.Tag.back_button).assertIsDisplayed()
-    composeTestRule.onNodeWithTag(C.Tag.search_input_field).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(SearchScreenTestTags.SEARCH_SCREEN).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(SearchScreenTestTags.TOP_BAR).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(SearchScreenTestTags.BACK_BUTTON).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(SearchScreenTestTags.SEARCH_FIELD).assertIsDisplayed()
 
-    // Wait until users/events have been loaded and UI updated
+    // Wait until users/events/organizations have been loaded and UI updated
     composeTestRule.waitUntil(timeoutMillis = 5_000) {
-      composeTestRule
-          .onAllNodesWithTag(C.Tag.user_search_result)
-          .fetchSemanticsNodes()
-          .isNotEmpty() &&
-          composeTestRule
-              .onAllNodesWithTag(C.Tag.event_search_result)
-              .fetchSemanticsNodes()
-              .isNotEmpty()
+      composeTestRule.onNodeWithTag(SearchScreenTestTags.USERS_RESULTS).isDisplayed() &&
+          composeTestRule.onNodeWithTag(SearchScreenTestTags.EVENTS_RESULTS).isDisplayed() &&
+          composeTestRule.onNodeWithTag(SearchScreenTestTags.ORGANIZATIONS_RESULTS).isDisplayed()
     }
 
-    composeTestRule.onNodeWithTag(C.Tag.user_search_result).assertIsDisplayed()
-    composeTestRule.onNodeWithTag(C.Tag.user_search_result_title).assertIsDisplayed()
-    composeTestRule.onNodeWithTag(C.Tag.event_search_result).assertIsDisplayed()
-    composeTestRule.onNodeWithTag(C.Tag.event_search_result_title).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(SearchScreenTestTags.USERS_RESULTS).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(SearchScreenTestTags.USERS_TITLE).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(SearchScreenTestTags.USERS_ROW).assertIsDisplayed()
+    composeTestRule
+        .onAllNodesWithTag(SearchScreenTestTags.USER_ROW_CARD)
+        .assertAll(hasClickAction())
+        .onFirst()
+        .assertIsDisplayed()
+    composeTestRule.onNodeWithTag(SearchScreenTestTags.USERS_COLUMN).assertIsNotDisplayed()
+    composeTestRule.onAllNodesWithTag(SearchScreenTestTags.USER_COLUMN_CARD).assertCountEquals(0)
+
+    composeTestRule.onNodeWithTag(SearchScreenTestTags.EVENTS_RESULTS).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(SearchScreenTestTags.EVENTS_TITLE).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(SearchScreenTestTags.EVENT_ROW).assertIsDisplayed()
+    composeTestRule
+        .onAllNodesWithTag(SearchScreenTestTags.EVENT_ROW_CARD)
+        .assertAll(hasClickAction())
+        .onFirst()
+        .assertIsDisplayed()
+    composeTestRule.onNodeWithTag(SearchScreenTestTags.EVENT_COLUMN).assertIsNotDisplayed()
+    composeTestRule.onAllNodesWithTag(SearchScreenTestTags.EVENT_COLUMN_CARD).assertCountEquals(0)
+
+    composeTestRule.onNodeWithTag(SearchScreenTestTags.ORGANIZATIONS_RESULTS).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(SearchScreenTestTags.ORGANIZATIONS_TITLE).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(SearchScreenTestTags.ORGANIZATIONS_ROW).assertIsDisplayed()
+    composeTestRule
+        .onAllNodesWithTag(SearchScreenTestTags.ORGANIZATION_ROW_CARD)
+        .assertAll(hasClickAction())
+        .onFirst()
+        .assertIsDisplayed()
+    composeTestRule.onNodeWithTag(SearchScreenTestTags.ORGANIZATIONS_COLUMN).assertIsNotDisplayed()
+    composeTestRule
+        .onAllNodesWithTag(SearchScreenTestTags.ORGANIZATION_COLUMN_CARD)
+        .assertCountEquals(0)
   }
 
   @Test
   fun testSearchUserDisplayed() {
-    composeTestRule.onNodeWithTag(C.Tag.search_input_field).performTextInput("user")
+    composeTestRule.onNodeWithTag(SearchScreenTestTags.SEARCH_FIELD).performTextInput("user")
     composeTestRule.waitForIdle()
 
     composeTestRule.waitUntil(timeoutMillis = 5_000) {
-      composeTestRule.onAllNodesWithTag(C.Tag.user_search_result).fetchSemanticsNodes().isNotEmpty()
+      composeTestRule.onNodeWithTag(SearchScreenTestTags.USERS_RESULTS).isDisplayed()
     }
 
-    composeTestRule.onNodeWithTag(C.Tag.search_screen).assertIsDisplayed()
-    composeTestRule.onNodeWithTag(C.Tag.back_button).assertIsDisplayed()
-    composeTestRule.onNodeWithTag(C.Tag.search_input_field).assertIsDisplayed()
-    composeTestRule.onNodeWithTag(C.Tag.user_search_result).assertIsDisplayed()
-    composeTestRule.onNodeWithTag(C.Tag.user_search_result_title).assertIsDisplayed()
-    composeTestRule.onNodeWithTag(C.Tag.event_search_result).assertIsNotDisplayed()
-    composeTestRule.onNodeWithTag(C.Tag.event_search_result_title).assertIsNotDisplayed()
+    composeTestRule.onNodeWithTag(SearchScreenTestTags.SEARCH_SCREEN).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(SearchScreenTestTags.BACK_BUTTON).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(SearchScreenTestTags.SEARCH_FIELD).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(SearchScreenTestTags.EVENTS_RESULTS).assertIsNotDisplayed()
+    composeTestRule.onNodeWithTag(SearchScreenTestTags.ORGANIZATIONS_RESULTS).assertIsNotDisplayed()
+
+    composeTestRule.onNodeWithTag(SearchScreenTestTags.USERS_RESULTS).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(SearchScreenTestTags.USERS_TITLE).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(SearchScreenTestTags.USERS_ROW).assertIsNotDisplayed()
+    composeTestRule.onNodeWithTag(SearchScreenTestTags.USERS_COLUMN).assertIsDisplayed()
+    composeTestRule
+        .onAllNodesWithTag(SearchScreenTestTags.USER_COLUMN_CARD)
+        .assertAll(hasClickAction())
+        .onFirst()
+        .assertIsDisplayed()
   }
 
   @Test
   fun testSearchEventDisplayed() {
-    composeTestRule.onNodeWithTag(C.Tag.search_input_field).performTextInput("sample")
+    composeTestRule.onNodeWithTag(SearchScreenTestTags.SEARCH_FIELD).performTextInput("sample")
     composeTestRule.waitForIdle()
 
     composeTestRule.waitUntil(timeoutMillis = 5_000) {
-      composeTestRule
-          .onAllNodesWithTag(C.Tag.event_search_result)
-          .fetchSemanticsNodes()
-          .isNotEmpty()
+      composeTestRule.onNodeWithTag(SearchScreenTestTags.EVENTS_RESULTS).isDisplayed()
     }
 
-    composeTestRule.onNodeWithTag(C.Tag.search_screen).assertIsDisplayed()
-    composeTestRule.onNodeWithTag(C.Tag.back_button).assertIsDisplayed()
-    composeTestRule.onNodeWithTag(C.Tag.search_input_field).assertIsDisplayed()
-    composeTestRule.onNodeWithTag(C.Tag.user_search_result).assertIsNotDisplayed()
-    composeTestRule.onNodeWithTag(C.Tag.user_search_result_title).assertIsNotDisplayed()
-    composeTestRule.onNodeWithTag(C.Tag.event_search_result).assertIsDisplayed()
-    composeTestRule.onNodeWithTag(C.Tag.event_search_result_title).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(SearchScreenTestTags.SEARCH_SCREEN).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(SearchScreenTestTags.BACK_BUTTON).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(SearchScreenTestTags.SEARCH_FIELD).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(SearchScreenTestTags.USERS_RESULTS).assertIsNotDisplayed()
+    composeTestRule.onNodeWithTag(SearchScreenTestTags.ORGANIZATIONS_RESULTS).assertIsNotDisplayed()
+
+    composeTestRule.onNodeWithTag(SearchScreenTestTags.EVENTS_RESULTS).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(SearchScreenTestTags.EVENTS_TITLE).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(SearchScreenTestTags.EVENT_ROW).assertIsNotDisplayed()
+    composeTestRule.onNodeWithTag(SearchScreenTestTags.EVENT_COLUMN).assertIsDisplayed()
+    composeTestRule
+        .onAllNodesWithTag(SearchScreenTestTags.EVENT_COLUMN_CARD)
+        .assertAll(not(hasText("Sample Event 0")))
+        .assertAll(hasClickAction())
+        .onFirst()
+        .assertIsDisplayed()
   }
 
   @Test
   fun testSearchEventDisplayedWhenNotStart() {
-    composeTestRule.onNodeWithTag(C.Tag.search_input_field).performTextInput("event")
+    composeTestRule.onNodeWithTag(SearchScreenTestTags.SEARCH_FIELD).performTextInput("event")
     composeTestRule.waitForIdle()
 
     composeTestRule.waitUntil(timeoutMillis = 5_000) {
-      composeTestRule
-          .onAllNodesWithTag(C.Tag.event_search_result)
-          .fetchSemanticsNodes()
-          .isNotEmpty()
+      composeTestRule.onNodeWithTag(SearchScreenTestTags.EVENTS_RESULTS).isDisplayed()
     }
 
-    composeTestRule.onNodeWithTag(C.Tag.search_screen).assertIsDisplayed()
-    composeTestRule.onNodeWithTag(C.Tag.back_button).assertIsDisplayed()
-    composeTestRule.onNodeWithTag(C.Tag.search_input_field).assertIsDisplayed()
-    composeTestRule.onNodeWithTag(C.Tag.user_search_result).assertIsNotDisplayed()
-    composeTestRule.onNodeWithTag(C.Tag.user_search_result_title).assertIsNotDisplayed()
-    composeTestRule.onNodeWithTag(C.Tag.event_search_result).assertIsDisplayed()
-    composeTestRule.onNodeWithTag(C.Tag.event_search_result_title).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(SearchScreenTestTags.SEARCH_SCREEN).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(SearchScreenTestTags.BACK_BUTTON).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(SearchScreenTestTags.SEARCH_FIELD).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(SearchScreenTestTags.USERS_RESULTS).assertIsNotDisplayed()
+    composeTestRule.onNodeWithTag(SearchScreenTestTags.ORGANIZATIONS_RESULTS).assertIsNotDisplayed()
+
+    composeTestRule.onNodeWithTag(SearchScreenTestTags.EVENTS_RESULTS).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(SearchScreenTestTags.EVENTS_TITLE).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(SearchScreenTestTags.EVENT_ROW).assertIsNotDisplayed()
+    composeTestRule.onNodeWithTag(SearchScreenTestTags.EVENT_COLUMN).assertIsDisplayed()
+    composeTestRule
+        .onAllNodesWithTag(SearchScreenTestTags.EVENT_COLUMN_CARD)
+        .assertAll(not(hasText("Sample Event 0")))
+        .assertAll(hasClickAction())
+        .onFirst()
+        .assertIsDisplayed()
   }
 
-  @Test
+    @Test
+    fun testSearchOrganizationDisplayed() {
+        composeTestRule.onNodeWithTag(SearchScreenTestTags.SEARCH_FIELD).performTextInput("organization")
+        composeTestRule.waitForIdle()
+
+        composeTestRule.waitUntil(timeoutMillis = 5_000) {
+            composeTestRule.onNodeWithTag(SearchScreenTestTags.ORGANIZATIONS_RESULTS).isDisplayed()
+        }
+
+        composeTestRule.onNodeWithTag(SearchScreenTestTags.SEARCH_SCREEN).assertIsDisplayed()
+        composeTestRule.onNodeWithTag(SearchScreenTestTags.BACK_BUTTON).assertIsDisplayed()
+        composeTestRule.onNodeWithTag(SearchScreenTestTags.SEARCH_FIELD).assertIsDisplayed()
+        composeTestRule.onNodeWithTag(SearchScreenTestTags.USERS_RESULTS).assertIsNotDisplayed()
+        composeTestRule.onNodeWithTag(SearchScreenTestTags.EVENTS_RESULTS).assertIsNotDisplayed()
+
+        composeTestRule.onNodeWithTag(SearchScreenTestTags.ORGANIZATIONS_RESULTS).assertIsDisplayed()
+        composeTestRule.onNodeWithTag(SearchScreenTestTags.ORGANIZATIONS_TITLE).assertIsDisplayed()
+        composeTestRule.onNodeWithTag(SearchScreenTestTags.ORGANIZATIONS_ROW).assertIsNotDisplayed()
+        composeTestRule.onNodeWithTag(SearchScreenTestTags.ORGANIZATIONS_COLUMN).assertIsDisplayed()
+        composeTestRule
+            .onAllNodesWithTag(SearchScreenTestTags.ORGANIZATION_COLUMN_CARD)
+            .assertAll(not(hasText("Sample Event 0")))
+            .assertAll(hasClickAction())
+            .onFirst()
+            .assertIsDisplayed()
+    }
+  // Test to be added when organization logic is finished
+  /*@Test
   fun testSearchNothingDisplayed() {
-    composeTestRule.onNodeWithTag(C.Tag.search_input_field).performTextInput("nothing")
+    composeTestRule.onNodeWithTag(SearchScreenTestTags.SEARCH_FIELD).performTextInput("nothing")
     composeTestRule.waitForIdle()
 
     // Wait to ensure recomposition finished even if no results
     composeTestRule.waitUntil(timeoutMillis = 5_000) { true }
 
-    composeTestRule.onNodeWithTag(C.Tag.search_screen).assertIsDisplayed()
-    composeTestRule.onNodeWithTag(C.Tag.back_button).assertIsDisplayed()
-    composeTestRule.onNodeWithTag(C.Tag.search_input_field).assertIsDisplayed()
-    composeTestRule.onNodeWithTag(C.Tag.user_search_result).assertIsNotDisplayed()
-    composeTestRule.onNodeWithTag(C.Tag.user_search_result_title).assertIsNotDisplayed()
-    composeTestRule.onNodeWithTag(C.Tag.event_search_result).assertIsNotDisplayed()
-    composeTestRule.onNodeWithTag(C.Tag.event_search_result_title).assertIsNotDisplayed()
-  }
+    composeTestRule.onNodeWithTag(SearchScreenTestTags.SEARCH_SCREEN).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(SearchScreenTestTags.BACK_BUTTON).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(SearchScreenTestTags.SEARCH_FIELD).assertIsDisplayed()
+    composeTestRule.onNodeWithTag(SearchScreenTestTags.USERS_RESULTS).assertIsNotDisplayed()
+    composeTestRule.onNodeWithTag(SearchScreenTestTags.USERS_TITLE).assertIsNotDisplayed()
+    composeTestRule.onNodeWithTag(SearchScreenTestTags.EVENTS_RESULTS).assertIsNotDisplayed()
+    composeTestRule.onNodeWithTag(SearchScreenTestTags.EVENTS_TITLE).assertIsNotDisplayed()
+  }*/
 }
