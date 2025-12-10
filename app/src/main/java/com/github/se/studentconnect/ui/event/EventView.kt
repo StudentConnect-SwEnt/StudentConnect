e8c3c75ce01948b2bb14a90d1b1d3e493e81f1da
tristan@archlaptop <tristan.bourquin@epfl.ch>
2025-12-10 15:49:58 +0100

    fix: finish price appearance in event view

--- app/src/main/java/com/github/se/studentconnect/ui/event/EventView.kt
+++ app/src/main/java/com/github/se/studentconnect/ui/event/EventView.kt
@@ -1030,17 +1030,22 @@ private fun WebsiteButton(currentEvent: Event.Public, context: Context) {
       modifier = Modifier.testTag(EventViewTestTags.VISIT_WEBSITE_BUTTON))
 }
 
-  ButtonIcon(
-      id = R.drawable.ic_share,
-      onClick = { DialogNotImplemented(context) },
-      modifier = Modifier.testTag(EventViewTestTags.SHARE_EVENT_BUTTON))
+    ButtonIcon(
+        id = R.drawable.ic_share,
+        onClick = { DialogNotImplemented(context) },
+        modifier = Modifier.testTag(EventViewTestTags.SHARE_EVENT_BUTTON))
 
-  Spacer(modifier = Modifier.fillMaxWidth())
-  Text(
-      text =
-          if (currentEvent.participationFee == null) "Free"
-          else currentEvent.participationFee!!.toString() + "CHF",
-      maxLines = 1)
+    Spacer(modifier = Modifier.weight(1f))
+
+    Text(
+        text =
+            if (currentEvent.participationFee == null) stringResource(R.string.event_free)
+            else
+                currentEvent.participationFee!!.toString() +
+                    stringResource(R.string.event_currency),
+        maxLines = 1,
+    )
+  }
 }
 
 @Composable
