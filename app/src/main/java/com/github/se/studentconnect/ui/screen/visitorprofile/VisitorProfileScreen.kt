bbc62a5d1be233aafc1d3a960beb7f4bb934c8c0
TristanBour <tristan.bourquin@epfl.ch>
2025-12-15 22:51:13 +0100

    fix: tests

--- app/src/main/java/com/github/se/studentconnect/ui/screen/visitorprofile/VisitorProfileScreen.kt
+++ app/src/main/java/com/github/se/studentconnect/ui/screen/visitorprofile/VisitorProfileScreen.kt
@@ -32,6 +32,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.testTag
