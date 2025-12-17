3416a9804140b381d62c5cb94ed29e0a4feffbdc
Rita Naimi <rita.naimi@epfl.ch>
2025-12-17 23:56:08 +0100

    fix(test): add missing onAllNodesWithTag import to EventViewTest

--- app/src/androidTest/java/com/github/se/studentconnect/ui/event/EventViewTest.kt
+++ app/src/androidTest/java/com/github/se/studentconnect/ui/event/EventViewTest.kt
@@ -7,6 +7,7 @@ import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertIsNotDisplayed
 import androidx.compose.ui.test.hasTestTag
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onAllNodesWithTag
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
